cab1849b59785bff6a3c31f2ebde16d4b5959afc
Salim Chemes <salim@mobomo.com>
2021-02-24 22:57:52 -0300

    sort and search

---
+++ src/app/services/planets.datasource.ts
@@ -0,0 +1,68 @@
+import { CollectionViewer, DataSource } from '@angular/cdk/collections';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { IPlanet } from '../models/planet';
+import { ApiService } from './api.service';
+
+export class PlanetsDataSource implements DataSource<IPlanet> {
+  private planetSubject = new BehaviorSubject<IPlanet[]>([]);
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+  public loading$ = this.loadingSubject.asObservable();
+  private planets: [] = [];
+
+  constructor(private apiService: ApiService) {}
+
+  loadPlanets() {
+    this.loadingSubject.next(true);
+
+    this.apiService
+      .getPlanets()
+      .pipe(finalize(() => this.loadingSubject.next(false)))
+      .subscribe((planets: any) => {
+        this.planets = planets;
+        this.planetSubject.next(planets);
+      });
+  }
+
+  sortPlanets(sortType: string, sortOrder: string) {
+    let sortedPlanets: [] = [];
+    if (this.isNumericSort(sortType)) {
+      sortedPlanets =
+        sortOrder === 'asc'
+          ? this.planets.sort((a, b) => a[sortType] - b[sortType])
+          : this.planets.sort((a, b) => b[sortType] - a[sortType]);
+    } else {
+      sortedPlanets =
+        sortOrder === 'asc'
+          ? this.planets.sort((a, b) => (a[sortType] > b[sortType] ? 1 : -1))
+          : this.planets.sort((a, b) => (a[sortType] < b[sortType] ? 1 : -1));
+    }
+
+    this.planetSubject.next(sortedPlanets);
+  }
+
+  searchPlanets(searchTerm: string) {
+    const filteredPlanets = this.planets.filter((planet: IPlanet) =>
+      planet.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    this.planetSubject.next(filteredPlanets);
+  }
+
+  private isNumericSort(sortType: string) {
+    return (
+      sortType === 'population' ||
+      sortType === 'orbital_period' ||
+      sortType === 'surface_water'
+    );
+  }
+
+  connect(collectionViewer: CollectionViewer): Observable<IPlanet[]> {
+    console.log('Connecting data source');
+    return this.planetSubject.asObservable();
+  }
+
+  disconnect(collectionViewer: CollectionViewer): void {
+    this.planetSubject.complete();
+    this.loadingSubject.complete();
+  }
+}
